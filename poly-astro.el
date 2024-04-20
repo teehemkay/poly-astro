390643219cbb1d4b8caee62bee628062263de9ed
tmk <tmk@exidia.com>
2024-04-20 14:44:41 +0200

    style: coding guidelines

--- poly-astro.el
+++ poly-astro.el
@@ -41,33 +41,33 @@
 (define-hostmode poly-astro-hostmode :mode 'html-mode)
 
 (define-innermode poly-astro-fm-innermode
-  :mode 'js-mode
-  :head-matcher "\\`[ \t\n]*---\n"
-  :tail-matcher "^---\n"
-  :head-mode 'fundamental-mode
-  :tail-mode 'fundamental-mode)
+                  :mode 'js-mode
+                  :head-matcher "\\`[ \t\n]*---\n"
+                  :tail-matcher "^---\n"
+                  :head-mode 'fundamental-mode
+                  :tail-mode 'fundamental-mode)
 
 (define-auto-innermode poly-astro-style-tag-lang-innermode
-  :head-matcher "<[[:space:]]*style[[:space:]]*lang=[[:space:]]*[\"'][[:space:]]*[[:alpha:]]+[[:space:]]*[\"'][[:space:]]*>\n"
-  :tail-matcher "</[[:space:]]*style[[:space:]]*[[:space:]]*>"
-  :mode-matcher (cons  "<[[:space:]]*style[[:space:]]*lang=[[:space:]]*[\"'][[:space:]]*\\([[:alpha:]]+\\)[[:space:]]*[\"'][[:space:]]*>" 1)
-  :head-mode 'host
-  :tail-mode 'host
-  :body-indent-offset 2)
+                       :head-matcher "<[[:space:]]*style[[:space:]]*lang=[[:space:]]*[\"'][[:space:]]*[[:alpha:]]+[[:space:]]*[\"'][[:space:]]*>\n"
+                       :tail-matcher "</[[:space:]]*style[[:space:]]*[[:space:]]*>"
+                       :mode-matcher (cons  "<[[:space:]]*style[[:space:]]*lang=[[:space:]]*[\"'][[:space:]]*\\([[:alpha:]]+\\)[[:space:]]*[\"'][[:space:]]*>" 1)
+                       :head-mode 'host
+                       :tail-mode 'host
+                       :body-indent-offset 2)
 
 (define-innermode poly-astro-style-innermode
-  :mode 'css-mode
-  :head-matcher "<[[:space:]]*style[[:space:]]*[[:space:]]*>\n"
-  :tail-matcher "</[[:space:]]*style[[:space:]]*[[:space:]]*>"
-  :head-mode 'host
-  :tail-mode 'host
-  :body-indent-offset 2)
+                  :mode 'css-mode
+                  :head-matcher "<[[:space:]]*style[[:space:]]*[[:space:]]*>\n"
+                  :tail-matcher "</[[:space:]]*style[[:space:]]*[[:space:]]*>"
+                  :head-mode 'host
+                  :tail-mode 'host
+                  :body-indent-offset 2)
 
 (define-polymode poly-astro
-  :hostmode 'poly-astro-hostmode
-  :innermodes '(poly-astro-fm-innermode
-                poly-astro-style-tag-lang-innermode
-                poly-astro-style-innermode))
+                 :hostmode 'poly-astro-hostmode
+                 :innermodes '(poly-astro-fm-innermode
+                               poly-astro-style-tag-lang-innermode
+                               poly-astro-style-innermode))
 
 (provide 'poly-astro)
 ;;; poly-astro.el ends here
